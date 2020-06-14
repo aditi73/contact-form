9cf7003a06cc1b20cc91484f505eff75966d9f6c
Aditi Thakur <a.thakur73.at@gmail.com>
2020-06-14 16:10:18 +0530

    contacts form API with routes

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,17 @@
+class ContactsController < ApplicationController
+	def create
+		contact = Contact.new(contact_params)
+		if contact.save
+			ContactMailer.welcome_email(contact).deliver_now
+			render json: {status: :success, contact: contact, message: "Created contact successfully"}
+		else
+			render json: {status: :success, contact: [], message: "Failed to create contact"}
+		end
+	end
+
+	private
+
+	def contact_params
+		params.require(:contact).permit(:first_name, :last_name, :email, :phone, :message)
+	end
+end
